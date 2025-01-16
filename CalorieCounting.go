29b866bc156ebe431bd997f0c52d76550ee3f5fe
drocha-apex <drocha@apexclearing.com>
2025-01-16 11:40:37 -0800

    Add Github Actions for syntax checking

--- CalorieCounting.go
+++ CalorieCounting.go
@@ -34,25 +34,32 @@ func initalizeElfs() [][]int {
 	return elfs
 }
 
-func MostFood(HungryElfs [][] int)  {
+func MostFood(HungryElfs [][]int) MaxElf {
 	maxCalElf := MaxElf{
-		 elfNum: 0, 
-		 elfCal: 0,
+		elfNum: 0,
+		elfCal: 0,
 	}
 	for i := 0; i < MAX_ELFS; i++ {
 
-
-		if 
-		maxCalElf.elfCal = ElfTotalCalorie(elfs[i])
+		if ElfTotalCalorie(HungryElfs[i]) > maxCalElf.elfCal {
+			maxCalElf.elfCal = ElfTotalCalorie(HungryElfs[i])
+			maxCalElf.elfNum = i + 1
+		}
 	}
-	return 
+	return maxCalElf
 }
 
 func main() {
 	elfs := initalizeElfs()
-
-	for i := 0; i < MAX_ELFS; i++ {
-		fmt.Print("Elf #", i + 1, " has a total of ", ElfTotalCalorie(elfs[i]), " Calories\n")
+	elfWithMostFood := MaxElf{
+		elfNum: 0,
+		elfCal: 0,
 	}
 
+	for i := 0; i < MAX_ELFS; i++ {
+		fmt.Print("Elf #", i+1, " has a total of ", ElfTotalCalorie(elfs[i]), " Calories\n")
+	}
+
+	elfWithMostFood = MostFood(elfs)
+	fmt.Print("\n\nElf #", elfWithMostFood.elfNum, " with the most calories has a total of ", elfWithMostFood.elfCal, "\n")
 }
